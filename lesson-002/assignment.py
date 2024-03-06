80c11f7d1e346941cba10fc645aee542c2cb0c17
akash rawat <akash.rawat@enine.school>
2024-03-06 14:47:23 +0530

    Comitted

---
+++ lesson-002/assignment.py
@@ -0,0 +1,24 @@
+
+def random_name():
+    """Write code to replace first_name and last_name with
+    non-blank values that are between 8 and 12 chars in length
+    First letter of both first_name and last_name should be capitalized
+    while all the rest of the letters must be in lowercase"""
+   
+
+
+def random_age():
+    """Return a random number between 18 and 81"""
+  
+
+def random_person():
+    """function to find random person"""
+    
+
+
+def getlist(num_people):
+    """
+    
+    """
+   
+
---
+++ lesson-010/README.md
@@ -0,0 +1,7 @@
+
+# lesson_10
+## Fetching Data
+
+If 'https://httpbin.org' has 4 urls, it can be said that it has 4 urls in depth $1^{st}$. Similarly, these 4 urls will have other urls inside them which will be depth $2^{nd}$
+
+Write a python program to fetch all the urls in 'https://httpbin.org' where depth can be `n`
